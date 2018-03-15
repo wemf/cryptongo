d10ef90ff09b674d8808d29dcae3b184f3d019b0
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-03-15 09:18:15 -0500

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,54 @@
+import pymongo
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+from flask import Flask, jsonify, request
+
+def get_db_connection(uri):
+    client = pymongo.MongoClient(uri)
+    return  client.cryptongo
+
+app = Flask(__name__)
+db_connection = get_db_connection('mongodb://localhost:27017')
+
+def get_documents():
+	params = {}
+	name = int(request.args.get('name', ''))
+	limit = int(request.args.get('limit', 0))
+	if name:
+		params.udpate({'name':name})
+	cursor = db_connection.tickers.find(
+		params, {'_id': 0, 'ticker_hash': 0}
+        ).limit(limit)
+	return list(cursor)
+
+def get_top20():
+	params = {}
+	name = int(request.args.get('name', ''))
+	limit = int(request.args.get('limit', 0))
+	if name:
+		params.udpate({'name': name})
+        params.update({'rank': {'$lte': 20} })
+	cursor = db_connection.tickers.find(
+		params, {'_id': 0, 'ticker_hash': 0}
+        ).limit(limit)
+	return list(cursor)
+
+def remove_currency():
+	params = {}
+    name = request.args.get('name', '')
+    if name:
+        params.update({'name': name})
+    else:
+        returnFalse
+    return db_connection.tickers.delete_many(
+        params
+    ).deleted_count
+
+@app.route("/")
+def index():
+    return jsonify(
+        {
+            'name': 'Cryptongo API'
+        }
+    )
