50d7c54b62337711197a9adc190928d2851c8a90
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-03-15 10:06:31 -0500

    add delete method for tickers

--- api/main.py
+++ api/main.py
@@ -13,10 +13,10 @@ db_connection = get_db_connection('mongodb://localhost:27017/') #Conexión a la 
 def get_documents():
 	params = {} #Defino un diccionario de datos
 	name = request.args.get('name', '') #Obtengo el campo "name", recibido por get. Si no existe, devuelve vacío
-	limit = int(request.args.get('limit', 0)) #Obtengo el campo "limit", recibido por get. Si no existe, devuelve 0. Fuerzo conversión a entero
+	limit = int(request.args.get('limit', 0)) #Obtengo el campo "limit", recibido por get. Si no existe, devuelve 0, omitiendo limit. Fuerzo conversión a entero
 
 	if name: #Si existe "name"
-		params.update({'name', name}) #Lo agrego al diccionario de datos
+		params.update({'name': name}) #Lo agrego al diccionario de datos
 
 	cursor = db_connection.tickers.find( #Busco tickers
 		params, #, según lo que recibí por get
@@ -29,7 +29,7 @@ def get_documents():
 def get_top20():
 	params = {} #Defino un diccionario de datos
 	name = request.args.get('name', '') #Obtengo el campo "name", recibido por get. Si no existe, devuelve vacío
-	limit = int(request.args.get('limit', 0)) #Obtengo el campo "limit", recibido por get. Si no existe, devuelve 0. Fuerzo conversión a entero
+	limit = int(request.args.get('limit', 0)) #Obtengo el campo "limit", recibido por get. Si no existe, devuelve 0, omitiendo limit. Fuerzo conversión a entero
 
 	if name: #Si existe "name"
 		params.update({'name', name}) #Lo agrego al diccionario de datos
@@ -49,13 +49,13 @@ def remove_currency():
 	name = request.args.get('name', '') #Obtengo el campo "name", recibido por get. Si no existe, devuelve vacío
 
 	if name: #Si existe "name"
-		params.update({'name', name}) #Lo agrego al diccionario de datos
+		params.update({'name': name}) #Lo agrego al diccionario de datos
 	else:
 		return False
 
 	return db_connection.tickers.delete_many( #Borro todos los documentos que coincidan con el dato recibido por get
 		params
-	).delete_count() #Retorna la cantida de documentos borrados
+	).deleted_count #Retorna la cantidad de documentos borrados
 
 @app.route("/") #Defino la ruta raíz para recibir peticiones
 def index(): #Cuando alguien pida esta ruta, se ejecutará esta función
@@ -67,4 +67,19 @@ def index(): #Cuando alguien pida esta ruta, se ejecutará esta función
 
 @app.route("/top20", methods=['GET']) #Defino ruta GET para top20
 def top20(): #Se ejecutará esta función cuando alguien entre a esa ruta
-	return jsonify(get_top20()) #Devuelvo un json con la lista de top20```
+	return jsonify(get_top20()) #Devuelvo un json con la lista de top20
+
+@app.route("/tickers", methods=['GET', 'DELETE']) #Defino ruta para obtener o eliminar tickers, recibiendo petición por GET o DELETE
+def tickers(): #Con la ruta, se ejecuta esta función
+	if request.method == "GET": #Si el método consultado por la ruta, es GET
+		return jsonify(get_documents()) #Devuelve un json con el resultado de tickers, según parámetros por get (Puede ser sin parámetros)
+	elif request.method == "DELETE": #En cambio, si el método consultado es por DELETE
+		result = remove_currency() #Se borran los tickers que coincidan con los parámetros recibidos por get (Los parámetros son obligatorios)
+		if result > 0:
+			return jsonify({ #Retornar un json con la respuesta
+				'text': 'Documentos eliminados'
+			}), 204 #Flask permite agregar otro parámetro al return, indicando el código de respuesta, en este caso, un "No hay contenido" (común en los borrados de datos)
+		else:
+			return jsonify({ #Retornar un json con la respuesta
+				'error': 'No se encontraron documentos'
+			}), 404 #Flask permite agregar otro parámetro al return, indicando el código de respuesta, en este caso, un "No encontrado"``
